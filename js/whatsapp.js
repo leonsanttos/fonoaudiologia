e90bf79c70d4c853c6ca16f31fe84039e7a762f3
Leonardo D. Santos <leonardosantos1016@gmail.com>
2025-05-05 21:44:30 -0300

    colocando a parte de outras informaçoes de forma condicional

--- js/whatsapp.js
+++ js/whatsapp.js
@@ -1,26 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const form = document.getElementById("form-agendamento");
-  
-    form.addEventListener("submit", function (e) {
-      e.preventDefault(); // Impede envio do form
-  
-      const nome = document.getElementById("nome").value;
-      const email = document.getElementById("email").value;
-      const telefone = document.getElementById("telefone").value;
-      const data = document.getElementById("data").value;
-      const mensagem = document.getElementById("mensagem").value;
-      const hora = document.getElementById("horario").value;
-  
-      const texto = `Olá meu nome é ${nome}` + 
-      ` as ${hora}` + `, e gostaria de verificiar a disponibilidade para uma avaliação na seguinte data ${data}` +
-                    `Segue minhas formas de contato E-mail: ${email}  ` +
-                    `Telefone: ${telefone}  ` +
-                    `Mensagem: ${mensagem}  `;
-  
-      const numeroWhatsApp = "5585985009105"; // ✅ Substitua pelo seu número com DDI e DDD
-      const link = `https://wa.me/${numeroWhatsApp}?text=${encodeURIComponent(texto)}`;
-  
-      window.open(link, "_blank");
-    });
+  const form = document.getElementById("form-agendamento");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault(); // Impede envio do form
+
+    const nome = document.getElementById("nome").value;
+    const email = document.getElementById("email").value;
+    const telefone = document.getElementById("telefone").value;
+    const data = document.getElementById("data").value;
+    const mensagem = document.getElementById("mensagem").value.trim();
+    const hora = document.getElementById("horario").value;
+
+    let texto = `Olá, meu nome é ${nome}` +
+      `, e eu gostaria de verificar a disponibilidade para uma avaliação na seguinte data ${data}` +
+      ` às ${hora}h.` +
+      ` Segue minhas formas de contato: E-mail: ${email}, Telefone: ${telefone}.`;
+
+    if (mensagem !== "") {
+      texto += ` Outras informações: ${mensagem}`;
+    }
+
+    const numeroWhatsApp = "5585985009105"; // ✅ Substitua pelo seu número com DDI e DDD
+    const link = `https://wa.me/${numeroWhatsApp}?text=${encodeURIComponent(texto)}`;
+
+    window.open(link, "_blank");
   });
-  
+});
